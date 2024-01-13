51c12d595b74e1e1de0b53806219c3c8a33cf87a
PabloLopes19 <pabloelopes@hotmail.com>
2024-01-12 23:13:08 -0300

    Adicionada lista

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 n
 >app/src/main/java/com/example/stateapplication/MainActivity.kt,2/7/272aceee5aad44fcc0b006e23ce5fb61860f668a
+D
+app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
--- app/src/main/java/com/example/stateapplication/MainActivity.kt
+++ app/src/main/java/com/example/stateapplication/MainActivity.kt
@@ -3,72 +3,35 @@ package com.example.stateapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val snackbarHostState = remember { SnackbarHostState() }
-            var stringState by remember { mutableStateOf("Aooo potencia") }
-            val scope = rememberCoroutineScope()
-
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                snackbarHost = {
-                    SnackbarHost(hostState = snackbarHostState)
-                },
-                ) {it ->
-                println(it)
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 30.dp)
-                ){
-                    TextField(
-                        value = stringState,
-                        label = {
-                            Text("Digite alguma coisa aqui")
-                        },
-                        onValueChange = {
-                            stringState = it
-                        },
-                        singleLine = true
-                    )
-
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            scope.launch {
-                                snackbarHostState.showSnackbar(
-                                    message = "$stringState"
-                                )
-                            }
-                        }
-                    ){
-                        Text("Click here")
-                    }
+            LazyColumn(
+            ) {
+                itemsIndexed(
+                    listOf("This", "is", "Kotlin", "With", "Jetpack", "Compose", "aoooooo", "potenciaaa", "potenciaaa", "potenciaaa", "potenciaaa", "potenciaaa", "potenciaaa", "potenciaaa")
+                ) {index, string ->
+                    Text(
+                        "$index - $string",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp)
+                    );
                 }
             }
         }
