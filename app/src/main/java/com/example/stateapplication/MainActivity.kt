f46b52209cce81641f253d47626c2c7ae5fab6e3
PabloLopes19 <pabloelopes@hotmail.com>
2024-01-12 22:43:01 -0300

    Criado um state dentro da aplicação com input, button e Snackbar

--- app/src/main/java/com/example/stateapplication/MainActivity.kt
+++ app/src/main/java/com/example/stateapplication/MainActivity.kt
@@ -3,67 +3,74 @@ package com.example.stateapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.sp
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val color = remember {
-                mutableStateOf(Color.Red);
-            }
+            val snackbarHostState = remember { SnackbarHostState() }
+            var stringState by remember { mutableStateOf("Aooo potencia") }
+            val scope = rememberCoroutineScope()
 
-            Column {
-                ColorBox(modifier = Modifier
-                    .weight(1f)
-                ) {
-                    color.value = it
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                snackbarHost = {
+                    SnackbarHost(hostState = snackbarHostState)
+                },
+                ) {it ->
+                println(it)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ){
+                    TextField(
+                        value = stringState,
+                        label = {
+                            Text("Digite alguma coisa aqui")
+                        },
+                        onValueChange = {
+                            stringState = it
+                        },
+                        singleLine = true
+                    )
+
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    message = "$stringState"
+                                )
+                            }
+                        }
+                    ){
+                        Text("Click here")
+                    }
                 }
-
-                Box(modifier = Modifier
-                    .background(color.value)
-                    .weight(1f)
-                    .fillMaxSize()
-                )
             }
         }
-    }
-}
-
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    updateColor: (Color) -> Unit
-) {
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.Yellow)
-            .clickable {
-                updateColor(
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        alpha = 1f,
-                    )
-                )
-            }
-    ) {
-        Text("Toque aqui para mudar a cor", color = Color.Black, fontSize = 18.sp, modifier = Modifier.fillMaxSize())
     }
 }
